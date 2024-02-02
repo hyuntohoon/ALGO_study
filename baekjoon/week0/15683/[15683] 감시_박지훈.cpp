00fc611f49d3beed28f85b26dc1d04551632e03e
parkjihoon <zlxlgus123@naver.com>
2024-02-02 16:30:34 +0900

    15683 c++ solution

---
+++ baekjoon/week0/15683/[15683] 감시_박지훈.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+int N, M;
+int arr[9][9];
+int cctvNum = 0;
+int cctvDir[9];
+int cctvVis[9];
+vector <pair<int,pair<int, int>>> V;
+int room[9][9];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int ans = 9876543210;
+
+void checkCctv(int x, int y, int dir) {
+	while (N > y && M > x && x >= 0 && y >= 0 && arr[y][x] != 6) {
+		arr[y][x] = 1;
+		x = x + dx[dir];
+		y = y + dy[dir];
+	}
+}
+
+
+void checkCctv2(int x, int y, int dir, int tempArr[][9]) {// 시작지점부터 dir 방향으로 직진
+	while (N > y && M > x && x >= 0 && y >= 0 && tempArr[y][x] != 6) {
+		tempArr[y][x] = 1;
+		x = x + dx[dir];
+		y = y + dy[dir];
+	}
+}
+
+void cctvRot() { // 방향 돌리고 실행
+	int tempArr[9][9];
+	copy(&arr[0][0], &arr[0][0] + 9 * 9, &tempArr[0][0]); //배열 복제
+
+	for (int i = 0; i < cctvNum; i++) {
+		int cctv = V[i].first;
+		int x = V[i].second.second;
+		int y = V[i].second.first;
+		int nowDir = cctvDir[i];
+		if (cctv == 1) {
+			checkCctv2(x, y, nowDir, tempArr);
+		}
+		if (cctv == 2) {
+			int nowDir2 = (nowDir + 2) % 4;
+			checkCctv2(x, y, nowDir, tempArr);
+			checkCctv2(x, y, nowDir2, tempArr);
+		}
+		if (cctv == 3) {
+			int nowDir2 = (nowDir + 3) % 4;
+			checkCctv2(x, y, nowDir, tempArr);
+			checkCctv2(x, y, nowDir2, tempArr);
+		}
+		if (cctv == 4) {
+			int nowDir2 = (nowDir + 2) % 4;
+			int nowDir3 = (nowDir + 3) % 4;
+			checkCctv2(x, y, nowDir, tempArr);
+			checkCctv2(x, y, nowDir2, tempArr);
+			checkCctv2(x, y, nowDir3, tempArr);
+		}
+	}
+	int result = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (tempArr[i][j] == 0) result++; // 감시받지 않는 구역만 카운트
+		}
+	}
+	ans = min(result, ans);
+}
+
+void go(int cnt) {
+	if (cnt == cctvNum) {
+		cctvRot();
+		return;
+	}
+	for (int j = 0; j < 4; j++) {
+		cctvDir[cnt] = j; // 현재 CCTV의 방향 설정
+		go(cnt + 1); // 다음 CCTV 방향 설정으로 넘어감
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int temp;
+			cin >> temp;
+			if (temp != 0 && temp != 6 && temp != 5) {
+				cctvNum++;
+				V.push_back(make_pair(temp,make_pair(i, j)));
+				continue;
+			}
+			if (temp == 5) arr[i][j] = 5; // 5번은 따로 처리함 왜냐하면 회전의 의미가 없기 때문에
+			else arr[i][j] = temp;
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {// 5번은 따로 처리함 배열에 추가하고, 이 배열을 복사하여 사용
+			if (arr[i][j] == 5) {
+				int nowDir = 0;
+				int nowDir2 = (nowDir + 2) % 4;
+				int nowDir3 = (nowDir + 3) % 4;
+				int nowDir4 = (nowDir + 1) % 4;
+				checkCctv(j, i, nowDir);
+				checkCctv(j, i, nowDir2);
+				checkCctv(j, i, nowDir3);
+				checkCctv(j, i, nowDir4);
+			}
+		}
+	}
+
+	go(0);
+
+	cout << ans << endl;
+}
